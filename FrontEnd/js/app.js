5760572f53b0a86e8f494ee593e3c046fc804ec2
Guinoob27 <azertylclavier27@hotmail.com>
2025-04-29 13:11:45 +0200

    Add filters(not finished)

--- FrontEnd/js/app.js
+++ FrontEnd/js/app.js
@@ -1,6 +1,38 @@
-
-async function getWorks() {
+async function getWorks(filter) {
+    document.querySelector(".gallery").innerHTML = "";
     const url = "http://localhost:5678/api/works";
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`);
+      }
+      const json = await response.json();
+      if (filter) {
+        const filtered = json.filter((data) => data.categoryId === filter);
+        for (let i = 0; i < filtered.length; i++) {
+          setFigure(filtered[i]);
+        }
+      } else {
+        for (let i = 0; i < json.length; i++) {
+          setFigure(json[i]);
+        }
+      }
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+  getWorks();
+  
+  function setFigure(data) {
+    const figure = document.createElement("figure");
+    figure.innerHTML = `<img src=${data.imageUrl} alt=${data.title}>
+                      <figcaption>${data.title}</figcaption>`;
+  
+    document.querySelector(".gallery").append(figure);
+  }
+  
+  async function getCategories() {
+    const url = "http://localhost:5678/api/categories";
     try {
       const response = await fetch(url);
       if (!response.ok) {
@@ -8,21 +40,22 @@ async function getWorks() {
       }
   
       const json = await response.json();
-      console.log(json);
-    
-    for(let i =0; i< json.length; i++){
-        setFigure(json[i]);
-    }
+      for (let i = 0; i < json.length; i++) {
+        setFilter(json[i]);
+      }
     } catch (error) {
       console.error(error.message);
     }
   }
-  getWorks ();
-
-    function setFigure(data) {
-        const figure = document.createElement("figure")
-        figure.innerHTML = `<img src=${data.imageUrl} alt=${data.title}>
-				<figcaption>${data.title}</figcaption>`;
-
-        document.querySelector(".gallery").append(figure);
-    }
+  getCategories();
+  
+  function setFilter(data) {
+    const div = document.createElement("div");
+    div.className = data.id;
+    div.addEventListener("click", () => getWorks(data.id));
+    // Demander au mentor si j'ai un autre choix que de mettre l'id commen nom de classe
+    div.innerHTML = `${data.name}`;
+    document.querySelector(".div-container").append(div);
+  }
+  
+  document.querySelector(".tous").addEventListener("click", () => getWorks());
